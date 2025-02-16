e4a9586e100fda44c1b466c56e8fc8ebbe5ecb59
Joe Williams <joew4119@gmail.com>
2025-02-16 14:52:59 +0000

    add resource, create JSCButton, create font generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import JSCButton from './JSCButton';
+import JSCButton from './components/JSCButton';
+import createFont from './utils/createFont';
 
 function App() {
+  let image = createFont();
+
   return (
     <div className="App">
-      <JSCButton width={200}>Single Player</JSCButton>
+      <JSCButton width={450}>Singleplayer</JSCButton>
+      <JSCButton width={450}>Multiplayer</JSCButton>
+      <JSCButton width={450}>Minecraft Realms</JSCButton>
     </div>
   );
 }
